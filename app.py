ac98e706764d58bb613a64415422e13126420fde
Veronica Lobkina <95447175+verosfbay@users.noreply.github.com>
2022-05-21 13:04:04 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -49,5 +49,13 @@ def precipitation():
    precip = {date: prcp for date, prcp in precipitation}
    return jsonify(precip)
 
+# Stations route
+@app.route("/api/v1.0/stations")
+
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
 
 
