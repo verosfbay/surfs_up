21c8a7e9dec4178d0989c1f5a38be7d3d8e6d855
Veronica Lobkina <95447175+verosfbay@users.noreply.github.com>
2022-05-21 12:59:26 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -25,4 +25,29 @@ session = Session(engine)
 # Set Up Flask
 app = Flask(__name__)
 
+# We can define the welcome route 
+@app.route("/")
+
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+#The next route we'll build is for the precipitation analysis. 
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
+
 
